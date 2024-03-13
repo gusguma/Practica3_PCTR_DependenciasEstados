1eed7863c3d1150698ba2121dd66872e97e30119
Gustavo Gutierrez <gusguma@gmail.com>
2024-03-13 22:52:40 +0100

    Se implementa ActividadSalidaPuerta.java #1

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,8 +1,32 @@
 package src.p03.c01;
 
-public class ActividadSalidaPuerta{
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	//
-	// TODO
-	//
+public class ActividadSalidaPuerta implements Runnable {
+
+	private static final int NUMSALIDAS = 20;
+	private final String puerta;
+	private final IParque parque;
+
+	public ActividadSalidaPuerta(final String puerta, final IParque parque) {
+		this.puerta = puerta;
+		this.parque = parque;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < NUMSALIDAS; i++) {
+			try {
+				this.parque.salirDelParque(this.puerta);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5) * 1000);
+			} catch (final InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
+			}
+		}
+	}
 }
