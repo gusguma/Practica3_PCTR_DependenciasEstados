d0ca596396d98ba0b1fce3f20ae9ddae4af61ea3
Gustavo Gutierrez <gusguma@gmail.com>
2024-03-17 12:01:11 +0100

    Se documenta la clase IParque #11

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -1,5 +1,12 @@
 package src.p03.c01;
 
+/**
+ * Interfaz que permite crear implementaciones que representan entradas y
+ * salidas a un parque
+ * 
+ * @author Gustavo Gutierrez Martin - ggm1007@alu.ubu.es
+ * @since 1.8
+ */
 public interface IParque {
 
 	void entrarAlParque(String puerta) throws InterruptedException;
