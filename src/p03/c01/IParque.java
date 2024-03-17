5d9e93079a44dd21179b4f7d41686060837651bf
Gustavo Gutierrez <gusguma@gmail.com>
2024-03-17 10:39:21 +0100

    Se incluye throws InterruptedException #8

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -2,8 +2,8 @@ package src.p03.c01;
 
 public interface IParque {
 
-	void entrarAlParque(String puerta);
+	void entrarAlParque(String puerta) throws InterruptedException;
 
-	void salirDelParque(String puerta);
+	void salirDelParque(String puerta) throws InterruptedException;
 
 }
