65858f93522d6e68fec37a5724d691ad0b0f6582
Gustavo Gutierrez <gusguma@gmail.com>
2024-03-17 10:43:41 +0100

    Se refactoriza y se implementa salirDelParque #6

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -1,92 +1,84 @@
 package src.p03.c01;
 
-import java.util.Enumeration;
 import java.util.Hashtable;
 
-public class Parque implements IParque{
+public class Parque implements IParque {
 
-
-	// TODO 
+	private static final String ENTRADA = "Entrada";
+	private static final String SALIDA = "Salida";
+	private static final int CAPACIDAD = 50;
 	private int contadorPersonasTotales;
-	private Hashtable<String, Integer> contadoresPersonasPuerta;
-	
-	
-	public Parque() {
-		contadorPersonasTotales = 0;
-		contadoresPersonasPuerta = new Hashtable<String, Integer>();
-	}
+	private final Hashtable<String, Integer> contadorEntradasPuerta;
+	private final Hashtable<String, Integer> contadorSalidasPuerta;
 
+	public Parque() {
+		this.contadorPersonasTotales = 0;
+		this.contadorEntradasPuerta = new Hashtable<String, Integer>();
+		this.contadorSalidasPuerta = new Hashtable<String, Integer>();
+	}
 
 	@Override
-	public void entrarAlParque(String puerta){		// TODO
-		
-		// Si no hay entradas por esa puerta, inicializamos
-		if (contadoresPersonasPuerta.get(puerta) == null){
-			contadoresPersonasPuerta.put(puerta, 0);
-		}
-		
-		// TODO
-				
-		
+	public synchronized void entrarAlParque(final String puerta) throws InterruptedException {
+		comprobarAntesDeEntrar();
 		// Aumentamos el contador total y el individual
-		contadorPersonasTotales++;		
-		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)+1);
-		
+		this.contadorPersonasTotales++;
+		this.contadorEntradasPuerta.put(puerta, this.contadorEntradasPuerta.getOrDefault(puerta, 0) + 1);
 		// Imprimimos el estado del parque
-		imprimirInfo(puerta, "Entrada");
-		
-		// TODO
-		
-		
-		// TODO
-		
+		imprimirInfo(puerta, ENTRADA);
+		checkInvariante();
+		notify();
 	}
-	
-	// 
-	// TODO Método salirDelParque
-	//
-	
-	
-	private void imprimirInfo (String puerta, String movimiento){
+
+	@Override
+	public synchronized void salirDelParque(final String puerta) throws InterruptedException {
+		comprobarAntesDeSalir();
+		// Aumentamos el contador total y el individual
+		this.contadorPersonasTotales--;
+		this.contadorSalidasPuerta.put(puerta, this.contadorSalidasPuerta.getOrDefault(puerta, 0) + 1);
+		// Imprimimos el estado del parque
+		imprimirInfo(puerta, SALIDA);
+		checkInvariante();
+		notify();
+	}
+
+	private void imprimirInfo(final String puerta, final String movimiento) {
 		System.out.println(movimiento + " por puerta " + puerta);
-		System.out.println("--> Personas en el parque " + contadorPersonasTotales); //+ " tiempo medio de estancia: "  + tmedio);
-		
+		System.out.println("--> Personas en el parque " + this.contadorPersonasTotales);
 		// Iteramos por todas las puertas e imprimimos sus entradas
-		for(String p: contadoresPersonasPuerta.keySet()){
-			System.out.println("----> Por puerta " + p + " " + contadoresPersonasPuerta.get(p));
-		}
+		this.contadorEntradasPuerta.entrySet().forEach(
+				entry -> System.out.println("----> Entradas por puerta " + entry.getKey() + " -> " + entry.getValue()));
+		// Iteramos por todas las puertas e imprimimos sus salidas
+		this.contadorSalidasPuerta.entrySet().forEach(
+				entry -> System.out.println("----> Salidas por puerta " + entry.getKey() + " -> " + entry.getValue()));
 		System.out.println(" ");
 	}
-	
-	private int sumarContadoresPuerta() {
-		int sumaContadoresPuerta = 0;
-			Enumeration<Integer> iterPuertas = contadoresPersonasPuerta.elements();
-			while (iterPuertas.hasMoreElements()) {
-				sumaContadoresPuerta += iterPuertas.nextElement();
-			}
-		return sumaContadoresPuerta;
+
+	private int sumarEntradasPuerta() {
+		return this.contadorEntradasPuerta.values().stream().reduce(Integer::sum).orElse(0);
 	}
-	
+
+	private int sumarSalidasPuerta() {
+		return this.contadorSalidasPuerta.values().stream().reduce(Integer::sum).orElse(0);
+	}
+
 	protected void checkInvariante() {
-		assert sumarContadoresPuerta() == contadorPersonasTotales : "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
-		// TODO 
-		// TODO
-		
-		
-		
+		assert sumarEntradasPuerta() - sumarSalidasPuerta() == this.contadorPersonasTotales
+				: "INV: La sumarEntradasPuerta de contadores de las puertas debe ser igual al valor del contador del parte";
+		assert this.contadorPersonasTotales <= CAPACIDAD : "INV: Superada la capacidad máxima del parque.";
+		assert this.contadorPersonasTotales >= 0 : "INV: No puede haber mas salidas que entradas en el parque";
+
 	}
 
-	protected void comprobarAntesDeEntrar(){
-		//
-		// TODO
-		//
+	protected void comprobarAntesDeEntrar() throws InterruptedException {
+		while (this.contadorPersonasTotales == CAPACIDAD) {
+			wait();
+		}
 	}
 
-	protected void comprobarAntesDeSalir(){
-		//
-		// TODO
-		//
+	protected void comprobarAntesDeSalir() throws InterruptedException {
+		while (this.contadorPersonasTotales == 0) {
+			wait();
+		}
 	}
-
 
 }
