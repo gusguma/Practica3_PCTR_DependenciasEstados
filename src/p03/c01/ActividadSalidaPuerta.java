0dfda81f349942fc593b458ca0b386146eb91ca3
Gustavo Gutierrez <gusguma@gmail.com>
2024-03-17 10:42:39 +0100

    Se modifica Random para mejor visualización #1

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -21,7 +21,7 @@ public class ActividadSalidaPuerta implements Runnable {
 		for (int i = 0; i < NUMSALIDAS; i++) {
 			try {
 				this.parque.salirDelParque(this.puerta);
-				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5) * 1000);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(40) * 100);
 			} catch (final InterruptedException e) {
 				Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
 				Logger.getGlobal().log(Level.INFO, e.toString());
